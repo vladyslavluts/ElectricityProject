a90cc9d87303b7179bc5ac6d737624716f721dc6
Vladyslav Luts <vladyslav.luts@gmail.com>
2016-08-09 18:49:58 +0300

    Add files via upload

---
+++ Electricity_Ui_/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+[assembly: OwinStartupAttribute(typeof(Electricity_Ui.Startup))]
+namespace Electricity_Ui
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            // AreaRegistration.RegisterAllAreas();
+           // RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
