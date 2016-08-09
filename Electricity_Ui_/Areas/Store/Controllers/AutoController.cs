7a1d6fa951eec3e038065b9667ae319baa97d4ec
Vladyslav Luts <vladyslav.luts@gmail.com>
2016-08-09 18:48:31 +0300

    Add files via upload

---
+++ Electricity_Ui_/Areas/Store/Controllers/AutoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Store.Controllers
+{
+    [Authorize]
+    public class AutoController : Controller
+    {
+        // GET: Store/Auto
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
